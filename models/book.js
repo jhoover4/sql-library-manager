ee0c8a86dd98ebff88805159633682d7e523d1b9
jhoover4 <jordan@hoovermld.com>
2019-11-10 16:35:35 -0600

    Add book routes and views.

--- models/book.js
+++ models/book.js
@@ -1,15 +1,31 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Book = sequelize.define('Book', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
-  }, {});
+  const Book = sequelize.define(
+    "Book",
+    {
+      title: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'Title is required'
+          }
+        }
+      },
+      author: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'Author is required'
+          }
+        }
+      },
+      genre: DataTypes.STRING,
+      year: DataTypes.INTEGER
+    },
+    {}
+  );
   Book.associate = function(models) {
     // associations can be defined here
   };
   return Book;
 };
-
-// TODO: Add appropriate validations to make sure that both title and author properties will have values when form is submitted
--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ const Book = require("../models").Book;
 
 /**
  * Route callback function.
- * 
+ *
  * @callback routeCallback
  * @param {req}
  * @param {res}
@@ -29,11 +29,105 @@ function asyncHandler(callback) {
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const books = await Book.findAll();
-    // TODO: Return these sorted by title
-    
-    res.render("books/index", { title: "Books" , books});
+    // TODO: Include a search file for the books listing page. Search should work for all fields. Should be case insensitive
+    //  and good for partial matches
+
+    // TODO: Include pagination for the books listing page
+    const books = await Book.findAll({ order: [["title"]] });
+
+    res.render("books/index", { title: "Books", books });
   })
 );
+
+/* GET new book form. */
+router.get(
+  "/new",
+  asyncHandler(async (req, res) => {
+    res.render("books/new", { title: "New Book" });
+  })
+);
+
+/* POST new book form. */
+router.post(
+  "/new",
+  asyncHandler(async (req, res) => {
+    try {
+      await Book.create(req.body);
+      res.redirect("/books");
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        res.render("books/new", {
+          errors: error.errors,
+          title: "New Book"
+        });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+/* GET book update form. */
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+
+    if (book) {
+      res.render("books/edit", { title: "Update Book", book });
+    } else {
+      req.sendStatus(404);
+    }
+  })
+);
+
+/* POST book update form. */
+router.post(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    let book;
+
+    try {
+      book = await Book.findByPk(req.params.id);
+
+      if (book) {
+        await book.update(req.body);
+        res.redirect("/books");
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        book = await Book.build(req.body);
+        book.id = req.params.id;
+        res.render("books/edit", {
+          book,
+          errors: error.errors,
+          title: "Update Book"
+        });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+/* Delete book. */
+router.post(
+  "/:id/delete",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+
+    if (book) {
+      await book.destroy();
+      res.redirect("/books");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+// TODO: Books/id not found should have a different error page than typical 404?
+// TODO: Setup a custom error handle middleware
 
 module.exports = router;
